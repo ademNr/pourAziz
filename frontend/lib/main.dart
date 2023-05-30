8722cd3cc773920fdaaa60fd8e21dba308d16242
ademnr <ademnr2@gmail.com>
2023-05-30 18:22:10 +0100

    recaptcha v2 changes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:frontend/view/auth/register_screen.dart';
+import 'package:frontend/view/auth/recaptchaPage.dart';
+import 'package:frontend/view/auth/signin.dart';
+import 'package:g_recaptcha_v3/g_recaptcha_v3.dart';
 import 'package:get/get.dart';
 
 import 'view/auth/reset.dart';
@@ -10,6 +12,7 @@ import 'view/auth/reset.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
 
   runApp(const MyApp());
 }
@@ -25,7 +28,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/page',
       theme: ThemeData(primaryColor: Color.fromARGB(255, 255, 20, 20)),
       getPages: [
-        GetPage(name: '/page', page: () => const login()),
+        GetPage(name: '/page', page: () => const Recaptcha()),
       ],
     );
   }
