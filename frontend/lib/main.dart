7a9389c6d8802b7c1565adf5bbff9cdd40b89b7b
Ahmed Nouria <ahmnouira@gmail.com>
2023-05-31 02:18:45 +0100

    feat: added firebase config for platforms

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/src/app.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // Add Firebase Initialization
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const App());
 }
